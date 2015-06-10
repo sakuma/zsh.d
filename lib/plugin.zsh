1cd4896aef8c0a0ed1d4058074adf08a3e9c9734
Naohiro Sakuma <nao.bear@gmail.com>
2015-06-10 11:44:05 +0900

    rm plugin 'auto-fu.zsh'

--- lib/plugin.zsh
+++ lib/plugin.zsh
@@ -1,32 +1,4 @@
 ############
-# auto-fu.zsh
-#
-auto_fu_file_path=$ZSH_PLUGIN/auto-fu.zsh/auto-fu.zsh
-if [ -f $auto_fu_file_path ]; then
-  ######
-  ## for generate
-  ## A=$auto_fu_file_path; (zsh -c "source $A ; auto-fu-zcompile $A ~/.zsh.d")
-  ##-------
-  if [ -f $ZSH_HOME/auto-fu ]; then
-    { . $ZSH_HOME/auto-fu; auto-fu-install; }
-  else
-    (zsh -c "source $auto_fu_file_path ; auto-fu-zcompile $auto_fu_file_path $ZSH_HOME")
-    { . $ZSH_HOME/auto-fu; auto-fu-install; }
-  fi
-
-  zstyle ':auto-fu:highlight' input bold
-  zstyle ':auto-fu:highlight' completion fg=black,bold
-  zstyle ':auto-fu:highlight' completion/one fg=white,bold,underline
-  zstyle ':auto-fu:var' postdisplay $'\n-azfu-'
-  zstyle ':auto-fu:var' track-keymap-skip opp
-  zle-line-init () {auto-fu-init;}; zle -N zle-line-init
-  zle -N zle-keymap-select auto-fu-zle-keymap-select
-else
-  echo "Not found 'auto-fu', -> 'git submodule init; git submodule update'"
-fi
-
-
-############
 # zaw.zsh
 #
 if [ -s $ZSH_PLUGIN/zaw/zaw.zsh ]; then
