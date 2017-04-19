e79f0ed55e51dd98761a4bce88180b68b23889e7
Naohiro Sakuma <nao.bear@gmail.com>
2017-04-20 01:06:01 +0900

    add function of git checkout

--- lib/utils.zsh
+++ lib/utils.zsh
@@ -141,3 +141,26 @@ function s() {
     }
   ' ~/.ssh/config | sort | peco)
 }
+
+# function git_checkout_peco () {
+#   git checkout $(git branch | sort | peco)
+# }
+# bindkey '^g' git_checkout_peco
+
+function peco-git-branch-checkout () {
+    local selected_branch_name="$(git branch -a | peco | tr -d ' ')"
+    case "$selected_branch_name" in
+        *-\>* )
+            selected_branch_name="$(echo ${selected_branch_name} | perl -ne 's/^.*->(.*?)\/(.*)$/\2/;print')";;
+        remotes* )
+            selected_branch_name="$(echo ${selected_branch_name} | perl -ne 's/^.*?remotes\/(.*?)\/(.*)$/\2/;print')";;
+    esac
+    if [ -n "$selected_branch_name" ]; then
+        BUFFER="git checkout ${selected_branch_name}"
+        zle accept-line
+    fi
+    zle clear-screen
+}
+zle -N peco-git-branch-checkout
+
+bindkey '^g' peco-git-branch-checkout
