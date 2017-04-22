367802550fa83fd1d90f4813689f6e7f52ca4494
Naohiro Sakuma <nao.bear@gmail.com>
2017-04-22 23:30:01 +0900

    remove exist check

--- plugin.zsh
+++ plugin.zsh
@@ -44,52 +44,44 @@ zplug load
 ############
 # zaw.zsh
 #
-if [ -s $ZPLUG_REPOS/zsh-users/zaw/zaw.zsh ]; then
-  autoload -Uz chpwd_recent_dirs cdr add-zsh-hook
-  add-zsh-hook chpwd chpwd_recent_dirs
-  zstyle ':chpwd:*' recent-dirs-max 5000
-  zstyle ':chpwd:*' recent-dirs-default yes
-  zstyle ':completion:*' recent-dirs-insert both
+autoload -Uz chpwd_recent_dirs cdr add-zsh-hook
+add-zsh-hook chpwd chpwd_recent_dirs
+zstyle ':chpwd:*' recent-dirs-max 5000
+zstyle ':chpwd:*' recent-dirs-default yes
+zstyle ':completion:*' recent-dirs-insert both
 
-  source $ZPLUG_REPOS/zsh-users/zaw/zaw.zsh
-  zstyle ':filter-select' case-insensitive yes
-  zstyle ':filter-select' rotate-list yes # enable rotation for filter-select
-  # zstyle ':filter-select:highlight' matched fg=yellow,standout
-  # zstyle ':filter-select' max-lines 10 # use 10 lines for filter-select
-  # zstyle ':filter-select' max-lines -10 # use $LINES - 10 for filter-select
-  zstyle ':filter-select' extended-search yes # see below
+source $ZPLUG_REPOS/zsh-users/zaw/zaw.zsh
+zstyle ':filter-select' case-insensitive yes
+zstyle ':filter-select' rotate-list yes # enable rotation for filter-select
+# zstyle ':filter-select:highlight' matched fg=yellow,standout
+# zstyle ':filter-select' max-lines 10 # use 10 lines for filter-select
+# zstyle ':filter-select' max-lines -10 # use $LINES - 10 for filter-select
+zstyle ':filter-select' extended-search yes # see below
 
-  bindkey '^X' zaw-cdr # Ctrl - x
-  bindkey '^R' zaw-history
-  bindkey '^O' zaw-open-file
-fi
+bindkey '^X' zaw-cdr # Ctrl - x
+bindkey '^R' zaw-history
+bindkey '^O' zaw-open-file
 
 ############
 # zsh-autosuggestions
 
-if [ -s $ZPLUG_REPOS/zsh-users/zsh-autosuggestions/zsh-autosuggestions.zsh ]; then
-  source $ZPLUG_REPOS/zsh-users/zsh-autosuggestions/zsh-autosuggestions.zsh
-  # use ctrl+t to toggle autosuggestions(hopefully this wont be needed)
-  # bindkey '^T' autosuggest-toggle
-  # use ctrl+f to accept a suggested word
-  # bindkey '^F' autosuggest-accept-suggested-word
-  bindkey '^F' autosuggest-accept
-fi
+source $ZPLUG_REPOS/zsh-users/zsh-autosuggestions/zsh-autosuggestions.zsh
+# use ctrl+t to toggle autosuggestions(hopefully this wont be needed)
+# bindkey '^T' autosuggest-toggle
+# use ctrl+f to accept a suggested word
+# bindkey '^F' autosuggest-accept-suggested-word
+bindkey '^F' autosuggest-accept
 
 ############
 # notify
 #
-if [ -s $ZPLUG_REPOS/marzocchi/zsh-notify/notify.plugin.zsh ]; then
-  source $ZPLUG_REPOS/marzocchi/zsh-notify/notify.plugin.zsh
-fi
+source $ZPLUG_REPOS/marzocchi/zsh-notify/notify.plugin.zsh
 
 
 ############
 # zsh syntax
 #
-if [ -s $ZPLUG_REPOS/zsh-users/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh ]; then
-  source $ZPLUG_REPOS/zsh-users/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
-fi
+source $ZPLUG_REPOS/zsh-users/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 
 ############
 # direnv
